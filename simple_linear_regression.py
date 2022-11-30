222cca50e15f9c9094b1181bea49783406e51f84
Mahesh <92411857+pimpalemahesh@users.noreply.github.com>
2022-11-30 09:50:14 +0530

    Update simple_linear_regression.py

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,63 +1,55 @@
 import numpy as np
+from statistics import mean
+from sklearn.linear_model import LinearRegression
 
 class Simple_Linear_Regression():
 
-    """
-        Class to calculate simple regression
-    """
-
     def __init__(self, X, Y):
-
-        """
-            Constructor:
-                Args :
-                X (list) : input value of feature
-                Y (list) : corresponding output values of feature.
-
-        """
         self.input = np.array(X)
         self.output = np.array(Y)
-
-        self.mean_x = sum(self.input)/len(self.input)
-        self.mean_y = sum(self.output)/len(self.output)
-
-        self.mean_sq_x = np.dot(self.input, self.input)/len(self.input)
-        self.mean_xy = np.dot(self.input, self.output)/len(self.input)
-
-        self.a1 = (self.mean_xy - self.mean_x*self.mean_y)/((self.mean_sq_x) - (self.mean_x)**2)
-
-        self.a0 = self.mean_y - self.a1*self.mean_x
-
-    def calculate_for_all(self, x_input):
-        """
-        Main Method:
-            Args:
-            x (float) : value of feature
-            Return:
-            output value of class y(float)
-        """
         
-        lst = [round(self.a1*x + self.a0, 2) for x in x_input]
-        return lst
+        self.a1 = (mean(self.input*self.output) - mean(self.input)*mean(self.output))/(mean(self.input**2) - mean(self.input)**2)
 
-    def calculate_for_one(self, x):
-        return round(self.a1*x + self.a0, 2)
-        
-    def root_mean_error(self, o_desired, o_predicted):
-        n = len(o_desired)
-        total = 0.0
-        for i in range(n):
-            total += (o_desired[i] - o_predicted[i])**2
-        return total/n
+        self.a0 = mean(self.output) - self.a1*mean(self.input)
 
-inputs = [6.2, 6.5, 5.4, 6.5, 7.1, 7.9, 8.5, 8.9, 9.5, 10.6]
-outputs = [26.3, 26.6, 25, 26, 27.9, 30.4, 35.4, 38.5, 42.6, 48.3]
+    def calculate(self, input):
+        return [self.a1 * x + self.a0 for x in input]
+    
+    def r_squared(self, y_output, y_predicted):
+        tss = sum((y - mean(y_output))**2 for y in y_output)
+        rss = sum((y - x)**2 for y,x in zip(y_output,y_predicted))
+        return 1 - (rss/tss)
+    
+    def mean_square_error(self, y, y_pred):
+        return sum([(x-y)**2 for x,y in zip(y, y_pred)])/len(y_pred)
 
+# inputs = [6.2, 6.5, 5.4, 6.5, 7.1, 7.9, 8.5, 8.9, 9.5, 10.6]
+# outputs = [26.3, 26.6, 25, 26, 27.9, 30.4, 35.4, 38.5, 42.6, 48.3]
 
-x = 10.6
+inputs = [1, 2, 3, 4, 5]
+outputs = [1.2, 1.8, 2.6, 3.2, 3.8]
+
+x = 7
 slr = Simple_Linear_Regression(inputs, outputs)
-ot = slr.calculate_for_all(inputs)
-print("My outputs : ")
-print(ot)
-error = slr.root_mean_error(outputs, ot)
-print("Root Mean square error : ",error)
+predicted_output = slr.calculate(inputs)
+print("Output : ", outputs)
+print("Predicted Output : ", predicted_output)
+print("R-Squared performance : ", slr.r_squared(outputs, predicted_output))
+print()
+print("Mean squared Error : " , slr.mean_square_error(outputs, predicted_output))
+print()
+
+
+X = [inputs]
+y = [outputs]
+
+lr = LinearRegression()
+lr.fit(X, y)
+print(lr.predict(X))
+print(slr.a0, slr.a1)
+
+
+
+
+
+        
